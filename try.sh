3a54813d0ced7e2ea2b3d8d1233728eb8b0c3a60
devanshmanu <devanshmanu@gmail.com>
2017-10-04 16:23:21 +0530

    new

--- try.sh
+++ try.sh
@@ -4,17 +4,15 @@ passwd="Yeti@4522"
 git add .
 git commit -m "$1"
 
-expect <<- DONE
 	
-	spawn git push;
+spawn git push;
 
-	expect "Username for 'https://github.com':" 
-	send "$usr\r"
+expect "Username for 'https://github.com':" 
+send "$usr\r"
 
-	expect "Password" 
-	send "$passwd\r"
+expect "Password" 
+send "$passwd\r"
 
-	expect "Initialization Sequence Completed"
+expect "Initialization Sequence Completed"
 
-DONE
 
