1ff47164d11ab57c7f53faab3e8d6aa14c8c048a
devanshmanu <devanshmanu@gmail.com>
2017-10-04 16:06:14 +0530

    new

---
+++ q3.sh
@@ -0,0 +1,14 @@
+automate
+1. git add .
+2. git commit -m <commandline argument>
+3. git push
+
+ex. write a script to automate the whole process,  which takes the commit message as input, 
+first part:
+put commit message and user name and password as variables and complete this.
+second part:
+take commit message as command line parameter
+third part:
+take username and password as read input (scanf alternative in bash)
+fourth part:
+hide password input when user enters it
---
+++ try.sh
@@ -0,0 +1,19 @@
+usr="devanshmanu"
+passwd="Yeti@4522"
+
+git add .
+git commit -m "$1"
+
+expect <<- DONE
+	
+	spawn git push;
+
+	expect "Username for 'https://github.com':" { send "$usr\r" }
+
+	expect "Password" { send "$passwd\r" }
+
+	expect "Initialization Sequence Completed"
+
+	interact;
+DONE
+
